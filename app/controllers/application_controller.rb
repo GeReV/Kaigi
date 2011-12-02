02fb9fd6736f598f7c16c766c2434993b3d6b864
Amir Grozki <amirgrozki@gmail.com>
2011-12-02 19:43:20 +0200

    Some extra crash-safety checks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ protected
   
   def init_fb
     @oauth = Koala::Facebook::OAuth.new(APP_CONFIG['app_id'], APP_CONFIG['app_secret'], APP_CONFIG['callback_url']) 
-    @graph = Koala::Facebook::API.new(@oauth.get_user_info_from_cookies(cookies)['access_token']) if @oauth
-    @current_user = @oauth.get_user_info_from_cookies(cookies)
+    
+    if (@oauth)
+      @graph = Koala::Facebook::API.new(@oauth.get_user_info_from_cookies(cookies)['access_token'])
+      @current_user = @oauth.get_user_info_from_cookies(cookies)
+    end
   end
 end
